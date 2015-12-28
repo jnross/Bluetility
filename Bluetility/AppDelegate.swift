f8bdf5b51911074249edc67b1c2f57e3e8a8eec7
Joseph Ross <jnross@gmail.com>
2015-12-27 22:52:31 -0800

    Improve icons.

--- Bluetility/AppDelegate.swift
+++ Bluetility/AppDelegate.swift
@@ -20,7 +20,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
-
+    
+    func applicationShouldTerminateAfterLastWindowClosed(sender: NSApplication) -> Bool {
+        return true
+    }
 
 }
 

4c9eb7ddae5fe2c56703c67df7e5c262080ed398
Joseph Ross <joe@junelife.com>
2020-04-04 11:32:16 -0700

    Fix autolayout issues when running on macOS Mojave (10.14).  Fixes #6.

--- Bluetility/BluetilityWindowController.swift
+++ Bluetility/BluetilityWindowController.swift
@@ -15,7 +15,7 @@ class BluetilityWindowController: NSWindowController {
     override func windowDidLoad() {
         super.windowDidLoad()
         self.shouldCascadeWindows = false
-        window?.setFrameAutosaveName("bluetility");
+        window?.setFrameAutosaveName("bluetility")
     
         // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
     }

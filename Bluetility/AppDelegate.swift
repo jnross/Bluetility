1b3218f973582c07457bb3639582006632522bd8
Joseph Ross <jnross@gmail.com>
2025-02-02 17:34:50 -0800

    Add AppDelegate method to eliminate warning.

--- Bluetility/AppDelegate.swift
+++ Bluetility/AppDelegate.swift
@@ -27,6 +27,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         return true
     }
+    
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        true
+    }
 
 }
 
